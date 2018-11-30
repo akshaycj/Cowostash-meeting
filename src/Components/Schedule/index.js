38d5806a4d42365b1870adf8f10ce3de97232259
Adhil Juvane <adhilmp3@gmail.com>
2018-11-30 22:19:38 +0530

    Schedule.js done

--- src/Components/Schedule/index.js
+++ src/Components/Schedule/index.js
@@ -2,23 +2,58 @@ import React from 'react' ;
 import './index.css' ;
 import {Input , Button , Icon , Calendar , Slider } from 'antd' ;
 import {Link} from 'react-router-dom'
+import moment from 'moment';
 
 export default class Screen3 extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       confirm : false ,
+      value : moment('2017-01-25') ,
+      name : "" ,
+      email : "" ,
+      companyName : "" ,
+      timeRequired : 0 ,
+      timeSlotSelected : 0 ,
     }
-  }
-
-  onPanelChange =(value, mode) => {
-    console.log(value, mode);
   }
 
   onConfirm = () => {
     this.setState({
       confirm : true ,
     })
+  }
+
+  onGoBack = () => {
+    this.setState({confirm : false})
+  }
+
+  onName = (name) => {
+    this.setState({name});
+  }
+
+  onEmail = (email) => {
+    this.setState({email});
+  }
+
+  onCompanyName = (company) => {
+    this.setState({companyName : company})
+  }
+
+  onSliderChange = (value) => {
+    this.setState({timeRequired : value})
+  }
+
+  onSelect = (value) => {
+    console.log("---",value.format('YYYY-MM-DD'));
+    this.setState({
+      value ,
+    })
+  }
+
+  timeSelect = (e) => {
+    console.log("e",e);
+    this.setState({timeSlotSelected : e})
   }
 
   render(){
@@ -28,21 +63,23 @@ export default class Screen3 extends React.Component{
     45: '45min' ,
     60: '60min',
   };
+  const { value, selectedValue } = this.state;
       if(this.state.confirm){
         return(
               <div className="screen3">
                 <div style={{width : "50%" , height : "100%" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection : "column"}}>
                   <Icon style={{fontSize : "100px"}} type="github" />
                   <div style={{fontSize : "30px" , padding : "auto" }}>Find a Time To meet Iryin</div>
-                  <Calendar fullscreen={false} onPanelChange={this.onPanelChange} />
+                  <Calendar value={value} fullscreen={false}/>
                 </div>
                 <div style={{width : "2px" , height : "80%" , margin : "auto" , backgroundColor : "#A9A9A9"}}></div>
                 <div style={{width : "50%" , height : "100%" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection : "column"}}>
                   <div style={{fontSize : "35px"}}>How Long Do You Need ?</div>
-                  <Slider style={{width : "200px"}} min={15} max={60} marks={marks} defaultValue={30} />
-                  <Button style={{width : "60%" , margin : "20px"}}> 4:00 AM </Button>
-                  <Button style={{width : "60%" , margin : "20px" , marginTop : "0px"}}> 10:00 PM </Button>
-                  <Button style={{width : "60%" , margin : "20px" , marginTop : "0px"}}> 2:00 PM </Button>
+                  <Slider style={{width : "200px"}} min={15} max={60} marks={marks} defaultValue={30} onChange={this.onSliderChange}/>
+                  <Button style={{width : "60%" , margin : "20px"}} onClick={() => this.timeSelect("4:00 AM")}> 4:00 AM </Button>
+                  <Button style={{width : "60%" , margin : "20px" , marginTop : "0px"}} onClick={() => this.timeSelect("10:00 PM")}> 10:00 PM </Button>
+                  <Button style={{width : "60%" , margin : "20px" , marginTop : "0px"}} onClick={() => this.timeSelect("2:00 PM")}> 2:00 PM </Button>
+                  <Button type="primary" onClick={this.onGoBack}>Go Back</Button>
                 </div>
               </div>
               )
@@ -53,15 +90,15 @@ export default class Screen3 extends React.Component{
                   <div style={{width : "50%" , height : "100%" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection : "column"}}>
                     <Icon style={{fontSize : "100px"}} type="github" />
                     <div style={{fontSize : "30px" , padding : "auto" }}>Find a Time To meet Iryin</div>
-                    <Calendar fullscreen={false} onPanelChange={this.onPanelChange} />
+                    <Calendar fullscreen={false} onSelect={this.onSelect}/>
                   </div>
                   <div style={{width : "2px" , height : "80%" , margin : "auto" , backgroundColor : "#A9A9A9"}}></div>
                   <div style={{width : "50%" , height : "100%" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection : "column"}}>
                     <div style={{fontSize : "35px"}}>Confirm meeting For</div>
-                    <div style={{fontSize : "20px"}}>Friday October 7 , 2018  2:00PM</div>
-                    <Input style={{width : "60%" , margin : "10px"}} placeholder="Full Name" />
-                    <Input style={{width : "60%" , margin : "10px" , marginTop : "0px"}} placeholder="Your Email ID" />
-                    <Input style={{width : "60%" , margin : "10px" , marginTop : "0px"}} placeholder="Company Name" />
+                    <div style={{fontSize : "20px"}}>{value.format('MMMM Do YYYY')}</div>
+                    <Input style={{width : "60%" , margin : "10px"}} placeholder="Full Name" onChange={this.onName}/>
+                    <Input style={{width : "60%" , margin : "10px" , marginTop : "0px"}} placeholder="Your Email ID" onChange={this.onEmail}/>
+                    <Input style={{width : "60%" , margin : "10px" , marginTop : "0px"}} placeholder="Company Name" onChange={this.onCompanyName}/>
                     <div style={{width : "100%" , display : "flex" , alignItems : "center" , justifyContent : "center"}}>
                       <Button type="primary" onClick={this.onConfirm}>Confirm</Button>
                       <Button style={{marginLeft : "20px"}} type="danger">Cancel</Button>
